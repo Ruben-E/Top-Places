20faf52c2dae7487d77c9e1419bd15c84e8f5124
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 11:46:11 +0200

    Created RecentsTableViewController

---
+++ Top Places/RecentsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecentsTableViewController.h
+//  Top Places
+//
+//  Created by Ruben Ernst on 29-04-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentsTableViewController : UITableViewController
+
+@end
