2d9487e34da3e236e008de3542a87726c5518738
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 13:54:40 +0200

    Created Picture model file

---
+++ Top Places/Picture.h
@@ -0,0 +1,13 @@
+//
+//  Picture.h
+//  Top Places
+//
+//  Created by Ruben Ernst on 29-04-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Picture : NSObject
+
+@end
