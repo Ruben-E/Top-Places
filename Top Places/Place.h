9a560b80efc1904d8b63f7276238acda5e2eb9f5
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 13:55:50 +0200

    Created Place model file

---
+++ Top Places/Place.h
@@ -0,0 +1,13 @@
+//
+//  Place.h
+//  Top Places
+//
+//  Created by Ruben Ernst on 29-04-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Place : NSObject
+
+@end
