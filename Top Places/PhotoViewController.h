187809d253c8648a3780d2f1e92346f9b31303da
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 11:52:38 +0200

    Created PhotoViewController

---
+++ Top Places/PhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.h
+//  Top Places
+//
+//  Created by Ruben Ernst on 29-04-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController
+
+@end
