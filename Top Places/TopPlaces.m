251b13ce481be1a05083dc5a765160b6d739af4b
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 13:56:47 +0200

    Created TopPlaces model file

---
+++ Top Places/TopPlaces.m
@@ -0,0 +1,13 @@
+//
+//  TopPlaces.m
+//  Top Places
+//
+//  Created by Ruben Ernst on 29-04-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import "TopPlaces.h"
+
+@implementation TopPlaces
+
+@end
