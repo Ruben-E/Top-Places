147a55d43c71c35ed83a00c217b4687e73cbee6c
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 13:56:22 +0200

    Created Country model file

---
+++ Top Places/Country.m
@@ -0,0 +1,13 @@
+//
+//  Country.m
+//  Top Places
+//
+//  Created by Ruben Ernst on 29-04-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import "Country.h"
+
+@implementation Country
+
+@end
