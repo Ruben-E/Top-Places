c6bdbfebd69951ef6f6243705a2c617691eac70c
Ruben Ernst <rubenernst@gmail.com>
2014-04-29 18:55:18 +0200

    Added extra fields (owner, uploaded_at) to the Picture model

--- Top Places/Picture.h
+++ Top Places/Picture.h
@@ -10,9 +10,18 @@
 
 @interface Picture : NSObject
 
+#define FLICKR_PHOTO_TITLE @"title"
+#define FLICKR_PHOTO_DESCRIPTION @"description._content"
+#define FLICKR_PHOTO_ID @"id"
+#define FLICKR_PHOTO_OWNER @"ownername"
+#define FLICKR_PHOTO_UPLOAD_DATE @"dateupload" // in seconds since 1970
+#define FLICKR_PHOTO_PLACE_ID @"place_id"
+
 @property (nonatomic) NSUInteger pictureId;
-@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSString *description;
+@property (nonatomic) NSUInteger owner;
+@property (nonatomic) NSUInteger uploaded_at;
 @property (nonatomic, strong) NSString *url;
 
 @end
