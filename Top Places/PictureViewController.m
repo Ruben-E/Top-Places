f1f4db8f4e083e0aed86695d7e813638c54d8966
Ruben Ernst <rubenernst@gmail.com>
2014-04-30 13:01:55 +0200

    Add the downloading of the image for the detailview to a background thread

--- Top Places/PictureViewController.m
+++ Top Places/PictureViewController.m
@@ -37,6 +37,32 @@
     _picture = picture;
     
     self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[FlickrFetcher URLforPhoto:self.picture.raw format:FlickrPhotoFormatLarge]]];
+    
+    [self startDownloadingImage];
+}
+
+- (void)startDownloadingImage {
+    self.image = nil;
+    
+    if (self.picture) {
+        NSURL *url = [FlickrFetcher URLforPhoto:self.picture.raw format:FlickrPhotoFormatLarge];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+            if (!error) {
+                if ([request.URL isEqual:url]) {
+                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        self.image = image;
+                    });
+                }
+            }
+        }];
+        
+        [task resume];
+    }
 }
 
 - (UIImage *)image {
