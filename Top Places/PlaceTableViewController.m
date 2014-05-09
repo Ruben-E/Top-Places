2e84245feb777f87155c9b05d5580c664d75c626
Ruben Ernst <rubenernst@gmail.com>
2014-05-09 08:36:59 +0200

    Helper method to make the segue generic for iPhone and iPad

--- Top Places/PlaceTableViewController.m
+++ Top Places/PlaceTableViewController.m
@@ -53,10 +53,7 @@
         Picture *picture = [self.place getPictureByRowNumber:indexPath.row];
         PictureViewController *pictureViewController = (PictureViewController *)detail;
         
-        
-        pictureViewController.pictureURL = picture.url;
-        pictureViewController.pictureTitle = picture.title;
-        pictureViewController.title = picture.title;
+        [self prepateImagePictureViewController:pictureViewController toDisplayPicture:picture];
     }
 }
 
@@ -88,43 +85,11 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)prepateImagePictureViewController:(PictureViewController *)pictureViewController toDisplayPicture:(Picture *)picture {
+    pictureViewController.pictureURL = picture.url;
+    pictureViewController.pictureTitle = picture.title;
+    pictureViewController.title = picture.title;
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Navigation
 
@@ -140,8 +105,7 @@
         Picture *picture = [self.place getPictureByRowNumber:indexPath.row];
         
         if (picture) {
-            pictureViewController.pictureURL = picture.url;
-            pictureViewController.pictureTitle = picture.title;
+            [self prepateImagePictureViewController:pictureViewController toDisplayPicture:picture];
         }
     }
 }
